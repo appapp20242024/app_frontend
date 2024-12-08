9198ed07278ab0abfed640b9a04bfb79b2726003
DoLongNghia <nghialong2610@gmail.com>
2024-12-08 18:57:37 +0700

    edit settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,10 +27,6 @@ include(":app")
 include(":compose-cardstack")
 
 check(JavaVersion.current().isCompatibleWith(JavaVersion.VERSION_17)) {
-    """
-    Quickmem requires JDK 17+ but it is currently using JDK ${JavaVersion.current()}.
-    Java Home: [${System.getProperty("java.home")}]
-    https://developer.android.com/build/jdks#jdk-config-in-studio
-    """.trimIndent()
+    .trimIndent()
 }
  
